579d0240920ce04a2313c1ed52fc4778153afd30
Joseph Wardell <jay@jaywardell.me>
2025-03-26 12:33:37 -0400

    created package. added methods for retrieving objects from NSItemProvider. added a convenience method for retrieving CGImage from an NSItemProvider

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "AsynchronousInterprocessCommunication"),
-
-    ]
+        
+    ], swiftLanguageModes: [.v5]
 )
---
+++ Sources/AsynchronousInterprocessCommunication/NSItemProvider+Images.swift
@@ -0,0 +1,61 @@
+//
+//  NSItemProvider+Images.swift
+//  Image Reader
+//
+//  Created by Joseph Wardell on 3/26/25.
+//
+
+import Foundation
+import CoreGraphics
+#if canImport(AppKit)
+import AppKit
+#elseif canImport(UIKit)
+import UIKit
+#endif
+import CoreGraphics
+
+@available(macOS 10.15, iOS 13, *)
+extension NSItemProvider {
+    
+    @MainActor
+    @preconcurrency
+    func getObject<T: NSObject>(type: T.Type) async throws -> sending T
+    where T: NSItemProviderReading {
+        try await withCheckedThrowingContinuation { continuation in
+            if canLoadObject(ofClass: T.self) {
+                loadObject(ofClass: T.self) { object, _ in
+                    guard let image = object as? T else {
+                        return continuation.resume(throwing: NSError())
+                    }
+                    let tosend: T = image.copy() as! T
+                    continuation.resume(returning: tosend)
+                }
+            }
+            else {
+                continuation.resume(throwing: NSError())
+            }
+        }
+    }
+    
+    @MainActor
+    func getCGImage() async throws -> CGImage? {
+        
+#if canImport(AppKit)
+        if #available(macOS 13.0, *) {
+            let image = try await getObject(type: NSImage.self)
+            guard let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)
+            else { return nil }
+            return cgImage
+        }
+        fatalError("\(#function) is only available in macOS 10.15 or iOS 13 or later")
+
+#elseif canImport(UIKit)
+        let image = try await getObject(type: UIImage.self)
+        guard let cgImage = image.cgImage
+        else { return nil }
+        
+        return cgImage
+#endif        
+    }
+}
+
